76f9a687bb2ab01c5070d74b3654d663c782fd8a
guilhermevrs <guilhermevrs@gmail.com>
2021-09-18 10:15:20 +0200

    feat: integrates dash and plotly

--- main.py
+++ main.py
@@ -3,28 +3,49 @@ from core.transaction_journal import TransactionJournal
 import piecash
 from piecash.core.transaction import ScheduledTransaction
 
-def main():
+import dash
+from dash import html, dcc
+import plotly.express as px
+import pandas as pd
+
+def piecash_test():
     print('# Opening file')
-
     book = piecash.open_book("/mnt/c/Users/guilh/Documents/Gnucash/test_sqllite/test_sqllite.gnucash")
-
     journal = TransactionJournal(book)
-
     transactions = journal.get_recorded_transactions(date(2021, 9, 10), date(2021, 9, 20))
-
     scheduled_transactions = journal.get_scheduled_transactions(date(2021, 9, 10), date(2021, 10, 20))
-
-    print(book.root_account)
-
     for acc in book.root_account.children:
         print(acc)
-
     scheduled = book.get(ScheduledTransaction)
-
     for tr in scheduled.all():
         print(tr)
+
+app = dash.Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Test'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main()
+    app.run_server(debug=True)
