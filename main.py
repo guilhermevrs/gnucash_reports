f605ece556252e24aa528f3072cc100c3a5f0a21
guilhermevrs <guilhermevrs@gmail.com>
2021-10-12 19:33:09 +0200

    feat: example of balance data

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from datetime import date
 from core.simple_transaction import SimpleTransaction, TransactionType
-from core.transaction_journal import TransactionJournal
+from core.transaction_journal import TransactionJournal, TransactionJournalConfig
 import piecash
 from piecash.core.transaction import ScheduledTransaction
 
 import dash
 from dash import html, dcc
-import plotly.express as px
-import pandas as pd
+import plotly.graph_objects as go
 
 # Scheduled: credit is when you lose, debit is when you receive
 
@@ -27,13 +26,16 @@ def dash_test():
 
     app = dash.Dash(__name__)
 
-    book = piecash.open_book("/mnt/c/Users/guilh/Documents/Gnucash/test_sqllite/test_sqllite.gnucash")
-    journal = TransactionJournal(book)
+    book = piecash.open_book("/mnt/c/Users/guilh/Documents/Gnucash/test_sqllite/test_sqllite.gnucash", open_if_lock=True)
+    config = TransactionJournalConfig(checkings_parent_guid="3838edd7804247868ebed2d2404d4c26")
+    journal = TransactionJournal(book=book, config=config)
 
     transaction_data = journal.get_transaction_data(date(2021, 10, 1), date(2021, 10, 30))
-    df = transaction_data.get_dataframe()
+    df = transaction_data.get_balance_data().sort_values(by="date")
 
-    fig = px.line(df.sort_values(by="date"), x="date", y="value", color="transaction_type", symbol="transaction_type", markers=True)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df["date"], y=df["balance"], name="Balance",
+                    line_shape='hv'))
 
     app.layout = html.Div(children=[
         html.H1(children='Test'),
@@ -52,5 +54,5 @@ def dash_test():
 
 if __name__ == "__main__":
     # execute only if run as a script
-    # dash_test()
-    piecash_test()
+    dash_test()
+    # piecash_test()
