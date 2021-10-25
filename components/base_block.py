901dfa0c07d0727697f95e81d390456bddd9baa4
guilhermevrs <guilhermevrs@gmail.com>
2021-10-25 15:37:55 +0200

    feat: base component

---
+++ components/base_block.py
@@ -0,0 +1,31 @@
+
+
+from dataclasses import dataclass
+from dash.dash import Dash
+from dash.development.base_component import Component
+
+
+@dataclass
+class BaseBlockConfig:
+    prefix: str
+
+
+class BaseBlock:
+    """
+    Defines a base component block for Dash
+    """
+
+    app: Dash
+    config: BaseBlockConfig
+    layout: Component
+
+    def __init__(self, app: Dash = None, config: BaseBlockConfig = None) -> None:
+        self.app = app
+        self.config = config
+
+        if self.app is not None and hasattr(self, "callbacks"):
+            self.callbacks()
+
+    def prefix(self, name: str) -> str:
+        """returns a prefixed name based on config"""
+        return "{}-{}".format(self.config.prefix, name) if self.config is not None else name
